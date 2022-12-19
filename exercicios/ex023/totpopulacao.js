bd2cf63a73576619e8bd26343a02c9f59c034ffc
Gabriel Mendes Rangel <gabrielmendesrangel2@gmail.com>
2022-12-19 20:26:13 -0300

    Tabela População Estados JavaScript

---
+++ exercicios/ex023/totpopulacao.js
@@ -0,0 +1,19 @@
+var listaNumPopulacoes = document.querySelectorAll('.numpopulacao');
+var totPopulacao = document.querySelector('.totpopulacao');
+var somaPopTot = 0;
+
+for (let i = 0; i < listaNumPopulacoes.length; i++) {
+    let numPopulacao = parseInt(listaNumPopulacoes[i].textContent.replace(/\./g, ""));
+
+    if(isNaN(numPopulacao)){
+        listaNumPopulacoes[i].textContent = 0;
+        numPopulacao = 0;
+    };
+
+    somaPopTot = somaPopTot + numPopulacao;
+};
+
+// totPopulacao.textContent = somaPopTot;
+// totPopulacao.textContent = totPopulacao.textContent.replace(/([0-9]{3})([0-9]{3})([0-9]{3})/g, '$1.$2.$3')
+
+totPopulacao.textContent = somaPopTot.toLocaleString('pt-br');
